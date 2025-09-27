5c71be4d46e14a7996dffdee0fca0c1ef3430dfd
Pranav206523 <pranavmishra1108@gmail.com>
2025-09-27 19:42:40 +0530

    Create sub.asm

---
+++ sub.asm
@@ -0,0 +1,93 @@
+global _start
+
+section .data
+    msg1 db "Enter 1st Value: ", 10
+    msg1len equ $- msg1
+
+    msg2 db "Enter 2nd Value: ", 10
+    msg2len equ $- msg2
+
+    msg3 db "The subtraction is: "
+    msg3len equ $- msg3
+
+    newline db 10
+
+section .bss
+    val1 resb 2
+    val2 resb 2
+    result resb 2
+
+section .text
+_start:
+    ; print first message
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg1
+    mov edx, msg1len
+    int 0x80
+
+    ; read first value
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, val1
+    mov edx, 2
+    int 0x80
+
+    ; print second message
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg2
+    mov edx, msg2len
+    int 0x80
+
+    ; read second value
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, val2
+    mov edx, 2
+    int 0x80
+
+    ; convert ASCII to numeric
+    mov al, [val1]
+    sub al, '0'
+    mov bl, [val2]
+    sub bl, '0'
+
+    ; subtraction
+    sub al, bl           ; AL = val1 - val2
+
+    ; divide by 10 to get tens and units
+    mov ah, 0
+    mov bl, 10
+    div bl               ; AL = quotient (tens), AH = remainder (units)
+
+    add al, '0'
+    mov [result], al
+    add ah, '0'
+    mov [result+1], ah
+
+    ; print result message
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg3
+    mov edx, msg3len
+    int 0x80
+
+    ; print result
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, result
+    mov edx, 2
+    int 0x80
+
+    ; print newline
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+
+    ; exit
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
