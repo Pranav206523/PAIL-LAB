02ff4e0a241e97483de50387507febeeaacd6926
Pranav206523 <pranavmishra1108@gmail.com>
2025-09-28 13:08:35 +0530

    Update addp.asm

--- addp.asm
+++ addp.asm
@@ -1,6 +1,12 @@
-global _start:
+global _start
+
 section .text
 _start:
-	mov ax , 30FAH
-	MOV bx , 595BH
-	add ax, bx
+    mov ax, 0x30FA      ; Load 0x30FA into AX
+    mov bx, 0x595B      ; Load 0x595B into BX
+    add ax, bx          ; AX = AX + BX
+
+    ; Exit syscall
+    mov eax, 1          ; sys_exit
+    int 0x80
+
