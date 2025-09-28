9e8a93cd72c66f2bc9cda8c095465d789acb71fa
Pranav206523 <pranavmishra1108@gmail.com>
2025-09-28 12:00:18 +0530

    Create aamsub.asm

---
+++ aamsub.asm
@@ -0,0 +1,47 @@
+global _start
+
+section .data
+    val db 0        ; units digit
+    vah db 0        ; tens digit
+    newline db 10
+
+section .text
+_start:
+    ; Load numbers
+    mov al, 10
+    mov bl, 8
+    sub al, bl      ; AL = 10 - 8 = 2
+
+    aam             ; AH = tens, AL = units (BCD adjust)
+
+    ; Convert to ASCII
+    add al, '0'
+    mov [val], al
+    add ah, '0'
+    mov [vah], ah
+
+    ; Print tens digit
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, vah
+    mov edx, 1
+    int 0x80
+
+    ; Print units digit
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, val
+    mov edx, 1
+    int 0x80
+
+    ; Print newline
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
+    int 0x80
+
+    ; Exit
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
