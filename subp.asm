d97b040c4003d315c46ef3a2a2128382b02119ce
Pranav206523 <pranavmishra1108@gmail.com>
2025-09-28 13:09:59 +0530

    Update subp.asm

--- subp.asm
+++ subp.asm
@@ -1,6 +1,11 @@
-global _start:
+global _start
+
 section .text
 _start:
-	mov ax , 595BH
-	MOV bx , 30FAH
-	sub ax, bx
+    mov ax, 0x595B      ; Load 0x595B into AX
+    mov bx, 0x30FA      ; Load 0x30FA into BX
+    sub ax, bx          ; AX = AX - BX
+
+    ; Exit syscall
+    mov eax, 1          ; sys_exit
+    int 0x80
